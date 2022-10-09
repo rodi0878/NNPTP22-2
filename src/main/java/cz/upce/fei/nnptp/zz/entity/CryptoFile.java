d51aea5dc457213c2ff54ee55840aece31784d4d
RackhmanCZ <matej.jelinek97@seznam.cz>
2022-10-09 20:54:15 +0200

    Fixed packages paths

--- src/main/java/cz/upce/fei/nnptp/zz/entity/CryptoFile.java
+++ src/main/java/cz/upce/fei/nnptp/zz/entity/CryptoFile.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cz.upce.fei.inptp.zz.entity;
+package cz.upce.fei.nnptp.zz.entity;
 
 import java.io.DataInput;
 import java.io.DataInputStream;
