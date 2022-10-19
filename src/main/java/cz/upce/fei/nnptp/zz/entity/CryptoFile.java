1c1fdafff3f21bc568b9a0364f9b69dfcd65ec99
Petr Bednář <p.petr.bednar@gmail.com>
2022-10-20 00:40:30 +0200

    Implemented AES encryption
    Passwords are defaultly encrypted to secret key using self as salt and PBKDF2WithHmacSHA256.
    Algorithm settings were chosen as AES/CBC/PKCS5Padding.
    Initialization vector used for AES set as static.

--- src/main/java/cz/upce/fei/nnptp/zz/entity/CryptoFile.java
+++ src/main/java/cz/upce/fei/nnptp/zz/entity/CryptoFile.java
@@ -1,29 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.upce.fei.nnptp.zz.entity;
 
-import java.io.DataInput;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
-import javax.crypto.CipherOutputStream;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
@@ -31,44 +32,54 @@ import javax.crypto.spec.SecretKeySpec;
  * @author Roman
  */
 public class CryptoFile {
-    
+
+    private static final String ALGORITHM = "AES/CBC/PKCS5Padding";
+    private static final Charset CHARSET = StandardCharsets.UTF_8;
+    private static final IvParameterSpec INITIALIZATION_VECTOR = new IvParameterSpec(new byte[16]);
+
+    private static SecretKey getKeyFromPassword(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), password.getBytes(), 65536, 256);
+        return new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+    }
+
+    private static String encrypt(String input, SecretKey key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key, INITIALIZATION_VECTOR);
+        byte[] cipherText = cipher.doFinal(input.getBytes());
+        return Base64.getEncoder().encodeToString(cipherText);
+    }
+
+    private static String decrypt(String cipherText, SecretKey key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key, INITIALIZATION_VECTOR);
+        byte[] plainText = cipher.doFinal(Base64.getDecoder().decode(cipherText));
+        return new String(plainText);
+    }
+
     public static String readFile(File file, String password) {
-        try(FileInputStream fileInputStream = new FileInputStream(file);) {
-            // TODO...
-            Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
-            CipherInputStream cipherInputStream = new CipherInputStream(fileInputStream, cipher);
-            SecretKey secretKey = new SecretKeySpec(password.getBytes(), "DES");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            
-            DataInputStream dataInputStream = new DataInputStream(cipherInputStream);
-            String result = dataInputStream.readUTF();
-            dataInputStream.close();
-            cipher.doFinal();
-            
-            return result;        
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IOException |
-                IllegalBlockSizeException | BadPaddingException ex) {
+
+        try {
+            Path path = Paths.get(file.getPath());
+            String data = Files.readAllLines(path, CHARSET).stream().collect(Collectors.joining());
+            return decrypt(data, getKeyFromPassword(password));
+        } catch (IOException | InvalidAlgorithmParameterException | InvalidKeyException | NoSuchAlgorithmException | InvalidKeySpecException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException ex) {
             Logger.getLogger(CryptoFile.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
         }
-        
-        return null;
     }
-    
-    public static void  writeFile(File file, String password, String contents) {
-        try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
-            Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
-            CipherOutputStream cipherOutputStream = new CipherOutputStream(fileOutputStream, cipher);
-            SecretKey secretKey = new SecretKeySpec(password.getBytes(), "DES");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            
-            DataOutputStream dataOutputStream = new DataOutputStream(cipherOutputStream);
-            dataOutputStream.writeUTF(contents);
-            dataOutputStream.close();
-            cipher.doFinal();
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IOException |
-                IllegalBlockSizeException | BadPaddingException ex) {
+
+    public static void writeFile(File file, String password, String contents) {
+
+        try {
+            Path path = Paths.get(file.getPath());
+            String data = encrypt(contents, getKeyFromPassword(password));
+            Files.write(path, Arrays.asList(data), CHARSET);
+        } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException | NoSuchPaddingException | InvalidAlgorithmParameterException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException ex) {
             Logger.getLogger(CryptoFile.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
 }
