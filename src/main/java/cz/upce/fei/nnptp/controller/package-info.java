5513a0f9777171992d46f178e0de3ea123cd2241
Lukáš Janáček <ljanacek@netvoice.cz>
2022-10-24 11:26:51 +0200

    Removed unnecessary .zz. package space.

--- src/main/java/cz/upce/fei/nnptp/controller/package-info.java
+++ src/main/java/cz/upce/fei/nnptp/controller/package-info.java
@@ -3,17 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cz.upce.fei.nnptp.zz.entity;
-
 /**
- *
- * @author Roman
+ * There should be controllers/logic for basic entity classes.
  */
-public enum ParameterType {
-    TITLE,
-    TEXT,
-    PASSWORD,
-    DATE
-
-    
-}
+package cz.upce.fei.nnptp.controller;
--- src/main/java/cz/upce/fei/nnptp/entity/ParameterType.java
+++ src/main/java/cz/upce/fei/nnptp/entity/ParameterType.java
@@ -3,9 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package cz.upce.fei.nnptp.entity;
+
 /**
- * This package should contain entity classes. Controllers, models and other logic shouldn't be handled there.
  *
- *
+ * @author Roman
  */
-package cz.upce.fei.nnptp.zz.entity;
+public enum ParameterType {
+    TITLE,
+    TEXT,
+    PASSWORD,
+    DATE
+
+    
+}
