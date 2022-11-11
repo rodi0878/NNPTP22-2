3ece850657525559344fb9c64e73fb57eafc329f
VanisTomas <vanis.t@seznam.cz>
2022-11-11 18:17:56 +0100

    Password class unit tests added

--- src/test/java/cz/upce/fei/nnptp/entity/PasswordTest.java
+++ src/test/java/cz/upce/fei/nnptp/entity/PasswordTest.java
@@ -11,13 +11,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashMap;
 
 /**
  *
  * @author Roman
  */
 public class PasswordTest {
-    
+
+    Password password;
+
     public PasswordTest() {
     }
     
@@ -31,6 +34,12 @@ public class PasswordTest {
     
     @BeforeEach
     public void setUp() {
+        password = new Password(1, "heslo1234", new HashMap<>() {
+            {
+                put("parameter1", new Parameter.TextParameter("value1"));
+                put("parameter2", new Parameter.TextParameter("value2"));
+            }
+        });
     }
     
     @AfterEach
@@ -38,10 +47,83 @@ public class PasswordTest {
     }
 
     @Test
-    public void testGetPassword() {
-        Password password = new Password(0, "pass");
-        
-        assertTrue(password.getPassword().equals("pass"));
+    public void testGetId() {
+        assertTrue(password.getId() == 1);
     }
-    
+
+    @Test
+    public void testGetPassword() {
+        assertTrue(password.getPassword().equals("heslo1234"));
+    }
+
+    @Test
+    public void testGetParameter() {
+        Parameter parameter1 = new Parameter.TextParameter("value1");
+        Parameter parameter2 = new Parameter.TextParameter("value3");
+        assertTrue(password.getParameter("parameter1").toString().equals(parameter1.toString()));
+        assertFalse(password.getParameter("parameter2").toString().equals(parameter2.toString()));
+    }
+
+    @Test
+    public void testGetParameters() {
+        HashMap<String, Parameter> map1 = new HashMap<>() {
+            {
+                put("parameter1", new Parameter.TextParameter("value1"));
+                put("parameter2", new Parameter.TextParameter("value2"));
+            }
+        };
+        HashMap<String, Parameter> map2 = new HashMap<>() {
+            {
+                put("parameter1", new Parameter.TextParameter("value1"));
+                put("parameter3", new Parameter.TextParameter("value2"));
+            }
+        };
+        assertTrue(password.getParameters().equals(map1));
+        assertFalse(password.getParameters().equals(map2));
+    }
+
+    @Test
+    public void testEquals() {
+        Password password1 = new Password(1, "heslo1234", new HashMap<>() {
+            {
+                put("parameter1", new Parameter.TextParameter("value1"));
+                put("parameter2", new Parameter.TextParameter("value2"));
+            }
+        });
+        Password password2 = new Password(1, "heslo123", new HashMap<>() {
+            {
+                put("parameter1", new Parameter.TextParameter("value1"));
+                put("parameter2", new Parameter.TextParameter("value2"));
+            }
+        });
+        Password password3 = new Password(1, "heslo1234", new HashMap<>() {
+            {
+                put("parameter1", new Parameter.TextParameter("value1"));
+                put("parameter2", new Parameter.TextParameter("value3"));
+            }
+        });
+        Password password4 = password;
+        assertTrue(password.equals(password1));
+        assertFalse(password.equals(password2));
+        assertFalse(password.equals(password3));
+        assertTrue(password.equals(password4));
+    }
+
+    @Test
+    public void testHasParameter() {
+        assertTrue(password.hasParameter("parameter1"));
+        assertFalse(password.hasParameter("parameter3"));
+    }
+
+    @Test
+    public void testHashCode() {
+        int expectedCode = 599197349;
+        assertTrue(password.hashCode() == expectedCode);
+    }
+
+    @Test
+    public void testToString() {
+        String expectedString = "Password{id=1, password=heslo1234, parameters=Parameter{key=parameter2, value=value2},Parameter{key=parameter1, value=value1}}";
+        assertTrue(password.toString().equals(expectedString));
+    }
 }
