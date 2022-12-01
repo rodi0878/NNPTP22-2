672ac3607d48cebe9caf50afc6606420a874c9ab
Viktor <Viktor@DESKTOP-DD1C8Q2>
2022-12-01 14:00:41 +0100

    Correction of previous solution adding support for multiple validation rules.

--- src/main/java/cz/upce/fei/nnptp/validation/Validator.java
+++ src/main/java/cz/upce/fei/nnptp/validation/Validator.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Validator<T> {
-    public final List<Validation<? super T>> validations = new ArrayList<>();
+    private final List<Validation<? super T>> validations = new ArrayList<>();
 
     public Validator(List<Validation<? super T>> validations) {
         this.validations.addAll(validations);
